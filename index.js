bffd57b56e19aea076efee74025bd2ce765d8468
Nora <noraonyi@gmail.com>
2022-03-26 14:04:21 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(Aki){
+    return `Hi, my name is ${Aki}.`;
+}
+ function introductionWithLanguage(name, language){
+return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+ }
+ // function introductionWithLanguage(Aki, Emberjs){
+     //return `Hi, my name is ${Aki} and I am learning to program in ${Emberjs}.`;
+ // }
+ function introductionWithLanguageOptional(name, language = "Javascript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+ }
+
+ 
+     
+ 
+
+
+    
